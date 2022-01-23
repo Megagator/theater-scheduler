59a2f2de07861965324c6440a093619e53889162
Mike Reinhard <mike@mikereinhard.com>
2022-01-23 15:21:30 -0800

    create and insert new events

--- scheduler/views.py
+++ scheduler/views.py
@@ -2,14 +2,57 @@ from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Theater, Movie
+import random, datetime
+
+from .models import MovieViewingEvent, Theater, Movie
 
 
 def index(request):
-    return render(request, "scheduler/index.html")
+    days = {}
+    day = datetime.date.today()
+    days[day.__str__()] = 'Today'
+    for _ in range(5):
+        day += datetime.timedelta(days=1)
+        days[day.__str__()] = day.strftime('%A, %b %-d')
+
+    minutes = []
+    for m in range(0, 60, 15):
+        minutes.append('{:02d}'.format(m))
+
+    events = MovieViewingEvent.objects.order_by('begins_at')
+    movies = Movie.objects.order_by('slug')
+    theaters = Theater.objects.order_by('short_name')
+    ctx = {
+        'events': events,
+        'movies': movies,
+        'theaters': theaters,
+        'random_movie': random.choice(movies).id,
+        'random_theater': random.choice(theaters).id,
+        'days': days,
+        'hours': range(24),
+        'minutes': minutes,
+        'random_day': random.randint(0, len(days)),
+        'random_hour': random.randint(0, 23),
+        'random_minute': random.choice(minutes)
+    }
+
+    return render(request, "scheduler/index.html", ctx)
 
 def create_event(request):
-    print(request.POST)
+    try:
+        time = ('{} {}:{}'.format(request.POST['event_day'], request.POST['event_hour'], request.POST['event_minute']))
+        date_format = '%Y-%m-%d %H:%M'
+        begins_at = datetime.datetime.strptime(time, date_format)
+
+        new_event = MovieViewingEvent(
+            movie_id = request.POST['movie_id'],
+            theater_id = request.POST['theater_id'],
+            begins_at = begins_at
+        )
+        new_event.save()
+    except Exception as e:
+        print(e)
+
     return HttpResponseRedirect(reverse('scheduler:index'))
 
 def theaters(request):
@@ -20,9 +63,7 @@ def theaters(request):
 
 def theater_schedule(request, theater_short_name):
     theater = get_object_or_404(Theater, short_name=theater_short_name)
-    ctx = {
-        'theater': theater
-    }
+    ctx = { 'theater': theater }
     return render(request, "scheduler/theater.html", ctx)
 
 def movies(request):
